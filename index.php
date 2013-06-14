6bd01bb93bd779bf3901ed1c526280b52195c279
Bachelor 3 <bachelor3@iMac-StO-202-163.local>
2013-06-14 11:45:12 +0200

    css style + nav responsive

--- index.php
+++ index.php
@@ -11,9 +11,8 @@
     	<ul id="navMenu">
     		<li><a href="#video">Accueil</a></li>
     		<li><a href="#adventure">L'aventure</a></li>
-    		<li><a href="#galerie">Galerie</a></li>
-    		<li><h1><img src="http://placekitten.com/g/200/300" width="200" height="200" title="" alt=""></h1></li>
     		<li><a href="#places">Lieux et Inscription</a></li>
+    		<li><h1><img src="http://placekitten.com/g/200/300" width="200" height="200" title="" alt=""></h1></li>
     		<li><a href="#about">A propos</a></li>
     		<li><a href="#contact">Contact</a></li>
     	</ul>
@@ -24,12 +23,19 @@
                   <option>--Select a page--</option>
                   <option value="#contact">Accueil</option>
                   <option value="#adventure">L'aventure</option>
-                  <option value="#galerie">Galerie</option>
                   <option value="#places">Lieux et Inscription</option>
                   <option value="#about">A propos</option>
                   <option value="#contact">Contact</option>
                 </select>
             </form>
+        </div>
+        <div id="social">
+        	<ul>
+        		<li><a href="">a</a></li>
+        		<li><a href="">b</a></li>
+        		<li><a href="">c</a></li>
+        		<li><a href="">d</a></li>
+        	</ul>
         </div>
     </nav>
     <div class="container">
@@ -37,25 +43,27 @@
             <iframe src="http://player.vimeo.com/video/34127945" id="video"></iframe>
         </div>
     </div>
-	<article id="adventure" style="height:1000px;">
+	<article id="adventure">
 	    <h2>L'aventure</h2>
 	    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium reprehenderit suscipit quos facilis ipsum at dolorem consequatur 
 	    quasi esse non nulla nihil voluptate neque totam aut sapiente id molestiae autem?</p>
 	    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste magnam commodi rem tempora sequi. Optio reprehenderit perspiciatis 
 	    in est quia dolores autem fuga alias sit officiis dolorum ad molestiae recusandae.</p>
 	</article>
-	<article id="galerie">
-	    <h2>Galerie</h2>
-	</article>
-	<article id="places">
+	<article id="places" class="clearfix">
 	    <h2>Lieux & Inscription</h2>
 	    <!-- google map iframe -->
-	    <iframe width="425" height="350" src="https://maps.google.com/?ie=UTF8&amp;t=m&amp;ll=34.01055,-118.171692&amp;spn=0.199214,0.291824&amp;z=11&amp;output=embed"></iframe>
-	    <br><a href="https://maps.google.com/?ie=UTF8&amp;t=m&amp;ll=34.01055,-118.171692&amp;spn=0.199214,0.291824&amp;z=11&amp;source=embed" style="color:#0000FF;text-align:left">Voir le plan sur Google maps</a>
-	    <form action="index.php" method="post">
-	    	<input type="email" placeholder="Email">
-	    	<input type="submit" value="Je m'inscris">
-	    </form>
+	    <section id="gMap">
+		    <iframe width="425" height="350" src="https://maps.google.com/?ie=UTF8&amp;t=m&amp;ll=34.01055,-118.171692&amp;spn=0.199214,0.291824&amp;z=11&amp;output=embed"></iframe>
+		    <br><a href="https://maps.google.com/?ie=UTF8&amp;t=m&amp;ll=34.01055,-118.171692&amp;spn=0.199214,0.291824&amp;z=11&amp;source=embed" style="color:#0000FF;text-align:left">Voir le plan sur Google maps</a>
+	    </section>
+	    <section id="mapForm">
+	    	<h3>Vivez l'aventure</h3>
+		    <form action="index.php" method="post">
+		    	<input type="email" placeholder="Email">
+		    	<input type="submit" value="Je m'inscris">
+		    </form>
+	    </section>
 	</article>
 	<article id="about">
 	    <h2>A propos</h2>
