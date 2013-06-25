3c6cdf2e9944292c16e406dc15bac999e0dae588
guillaume leclercq <dftl12@iMac-de-guillaume-leclercq.local>
2013-06-25 19:44:15 +0200

    font

--- index.php
+++ index.php
@@ -25,8 +25,8 @@
 			</div>
 			<div class="headerContent">
     			<h1>Back to the wild</h1>
-    			<h2>Partez en homme</h2>
-    			<h3>Revenez en heros !</h3>
+    			<h2 class="slanted">Partez en homme</h2>
+    			<h3 class="slanted">Revenez en heros !</h3>
     			<a href="">Regarder la video</a>
 			</div>
 			<div class="container home">
@@ -55,15 +55,17 @@
 		        <input type="submit" value="Envoyer" class="submit"/>
 		    </form>
 		</div>
-		<div class="section concept">
+		<div class="section concept clearfix">
 		    <h2>Concept de l'emission</h2>
-		    <div class="imgLeft"></div>
+		    <div class="imgLeft">
+		    	<img src="images/concept.jpg" alt="" height="429" width="416"/>
+		    </div>
 		    <div class="txtRight">
 		        <p>Back To The Wild vous permet de vous retrouver seul face à la nature.Les plus motivés seront sélectionnés pour participer à cette aventure unique.</p>
                 <p>Différents parcours proposés, dans différents pays, diffèrents climats, et de différentes difficultés.</p>
                 <p>Vous serez parachuté en pleine nature avec pour seule aide un gps, une ration de survie et devrez rallié un point le plus rapidement possible.</p>
                 <p>Survie et dépassement de soit sont les maîtres mots de cette expérience hors du commun.</p>
-                <p>L’equipe Back To the wild</p>
+                <p class="signature">L’equipe Back To the wild</p>
             </div>
 		</div>
 		<footer>
@@ -71,15 +73,15 @@
     		    <h2>Plus d'informations</h2>
     		    <div class="leftInfo">
     		    	<ul>
-    		    		<li><a href="">contact@backtothewild.fr</a></li>
-    		    		<li><a href="">@Backtothewild</a></li>
-    		    		<li><a href="">fb.com/Backtothewild</a></li>
+    		    		<li><a href="" class="contactLink">contact@backtothewild.fr</a></li>
+    		    		<li><a href="" class="twLink">@Backtothewild</a></li>
+    		    		<li><a href="" class="fbLink">fb.com/Backtothewild</a></li>
     	    		</ul>
         		</div>
         		<div class="rightInfo">
     	    		<form action="index.php#contact" method="post" class="contactForm">
-    	    			<input type="text" placeholder="Nom*" />
-    	    			<input type="text" placeholder="Email*" />
+    	    			<input type="text" placeholder="Nom*"  class="contactName"/>
+    	    			<input type="text" placeholder="Email*" class="contactMail"/>
     	    			<textarea name="" cols="78" rows="10" placeholder="Votre message"></textarea>
     	    			<input type="submit" value="Envoyer" class="submit"/>
     	    		</form>
